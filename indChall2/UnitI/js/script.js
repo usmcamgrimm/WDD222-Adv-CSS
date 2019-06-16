1a51ec9c0fb48f243949568de8d751f1cfc24f2c
usmcamgrimm <usmcamgrimm@gmail.com>
2019-06-16 12:48:49 -0400

    Get started on Unit I

---
+++ indChall2/UnitI/js/script.js
@@ -0,0 +1,26 @@
+/* 
+   Lakeland Reeds Bed and Breakfast menu toggle script 
+   Filename: script.js
+   HTML5 and CSS3 Illustrated Unit I, Skills Review
+ */
+
+var navButton = document.querySelector(".navigation-menu-button img");
+if (navButton.addEventListener) {
+   navButton.addEventListener("click", function() {
+      var nav = document.querySelector(".sitenavigation ul");
+      if (nav.style.display === "block") {
+         nav.style.display = "";
+      } else {
+         nav.style.display = "block";
+      }
+   }, false);
+} else if (navButton.attachEvent) {
+   navButton.attachEvent("onclick", function() {
+      var nav = document.querySelector(".sitenavigation ul");
+      if (nav.style.display === "block") {
+         nav.style.display = "";
+      } else {
+         nav.style.display = "block";
+      }
+   });
+}
